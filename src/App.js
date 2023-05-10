543f824ab41ee1d942087cecae9cca7a2b943447
Mito Youhei <mitoyouhei@proton.me>
2023-05-10 10:48:33 +0800

    add ui component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import { createTodo } from "./graphql/mutations";
 import { listTodos } from "./graphql/queries";
 
 import awsExports from "./aws-exports";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Text,
+  TextField,
+  View,
+} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
 Amplify.configure(awsExports);
 
 const initialState = { name: "", description: "" };
 
-const App = () => {
+const App = ({ signOut, user }) => {
   const [formState, setFormState] = useState(initialState);
   const [todos, setTodos] = useState([]);
 
@@ -44,30 +54,34 @@ const App = () => {
   }
 
   return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input
+    <View style={styles.container}>
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button style={styles.button} onClick={signOut}>
+        Sign out
+      </Button>
+      <Heading level={2}>Amplify Todos</Heading>
+      <TextField
+        placeholder="Name"
         onChange={(event) => setInput("name", event.target.value)}
         style={styles.input}
-        value={formState.name}
-        placeholder="Name"
+        defaultValue={formState.name}
       />
-      <input
+      <TextField
+        placeholder="Description"
         onChange={(event) => setInput("description", event.target.value)}
         style={styles.input}
-        value={formState.description}
-        placeholder="Description"
+        defaultValue={formState.description}
       />
-      <button style={styles.button} onClick={addTodo}>
+      <Button style={styles.button} onClick={addTodo}>
         Create Todo
-      </button>
+      </Button>
       {todos.map((todo, index) => (
-        <div key={todo.id ? todo.id : index} style={styles.todo}>
-          <p style={styles.todoName}>{todo.name}</p>
-          <p style={styles.todoDescription}>{todo.description}</p>
-        </div>
+        <View key={todo.id ? todo.id : index} style={styles.todo}>
+          <Text style={styles.todoName}>{todo.name}</Text>
+          <Text style={styles.todoDescription}>{todo.description}</Text>
+        </View>
       ))}
-    </div>
+    </View>
   );
 };
 
@@ -99,4 +113,4 @@ const styles = {
   },
 };
 
-export default App;
+export default withAuthenticator(App);
